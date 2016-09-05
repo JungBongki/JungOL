aab2f87d4289779d006d8d638c88adbd7c275839
정봉기 <yesjbk115@gmail.com>
2016-09-05 16:13:49 +0900

    빙고다시해보기

---
+++ src/Main1031.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class Main1031 {
+	
+	static int ROW = 5;
+	static int COL = 5;
+	static int[][] correct_pan = new int[ROW][COL];
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int[][] bingo = new int[ROW][COL];
+		
+		int result = 0;
+		
+		for(int i=0; i<ROW; i++){
+			for(int j=0; j<COL; j++){
+				bingo[i][j] = sc.nextInt();
+			}
+		}
+		for(int i=0; i<ROW; i++){
+			for(int j=0; j<COL; j++){
+				correct_pan[i][j] = 0;
+			}
+		}
+		
+		for(int count=0; count<ROW*COL; count++){
+			int cor = sc.nextInt();
+			for(int i=0; i<ROW; i++){
+				for(int j=0; j<COL; j++){
+					if(cor == bingo[i][j]){
+						correct_pan[i][j] = 1;
+					}
+					System.out.print(correct_pan[i][j] + " ");
+				}
+				System.out.println();
+			}
+			int chk = count_pan(correct_pan);
+			if(chk == 3){
+				result = count+1;
+				break;
+			}
+		}
+		sc.close();
+		
+		System.out.println(result);
+		
+	
+		
+		/*for(int i=0; i<ROW; i++){
+			for(int j=0; j<COL; j++){
+				System.out.print(correct_pan[i][j] + " ");
+			}
+			System.out.println();
+		}*/
+	}
+
+	private static int count_pan(int[][] correct_pan) {
+		int dia_L_count=0, dia_R_count=0;
+		int chk = 0;
+		for(int i=0; i<ROW; i++){
+			int row_count=0, col_count=0;
+			for(int j=0; j<COL; j++){
+				if(correct_pan[i][j]==1){
+					row_count++;
+				}
+				if(correct_pan[j][i]==1){
+					col_count++;
+				}
+				if(row_count==5){
+					chk++;
+				} else if(col_count==5){
+					chk++;
+				}
+			}
+		} // 가로세로
+		
+		
+		for(int i=0; i<ROW; i++){
+			if(correct_pan[i][i]==1){
+				dia_L_count++;
+				if(dia_L_count==5){
+					chk++;
+				}
+			}
+			if(correct_pan[i][COL-i-1]==1){
+				dia_R_count++;
+				if(dia_R_count==5){
+					chk++;
+				}
+			}
+		} //대각선
+		
+		System.out.println(chk);
+		return chk;
+	}
+}
