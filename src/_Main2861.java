e5c290eed39787d1f7bad514066933f7f9a1097f
Jungbongki <yesjbk1155@gmail.com>
2016-12-09 10:22:32 +0900

    수행시간 최적화 필요

--- src/_Main2861.java
+++ src/_Main2861.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -9,34 +7,41 @@ import java.util.Scanner;
  *  설명 : 3의 배수이거나 숫자에 3,6,9가 들어가 있으면 count
  *  
  */
-public class _Main2861 {
-	public static void main(String[] args){
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String A = ""; 
-		try {
-			A = br.readLine();
-		} catch (IOException e) {
-			// TODO 자동 생성된 catch 블록
-			e.printStackTrace();
-		}
-		String[] arg = A.split(" ");
-	}
-}
 
-/*
- * public static void main(String[] args) { Scanner sc = new Scanner(System.in);
- * 
- * 
- * int A = sc.nextInt(); int B = sc.nextInt(); int count = 0; int flag = 0;
- * while(A <= B){ flag = wh(A, 10); if(A%3==0 || flag == 1){ count++; } A++; }
- * 
- * for(int i=A; i<=B; i++){
- * 
- * 
- * boolean flag = Contain369(i); if(i % 3 == 0 || flag){ count++; } }
- * System.out.println(count % 20150523); }
- * 
- * private static int wh(int a, int k) { if(a % k == 3 || a % k == 6 || a % k ==
- * 9){ return 1; } if(a / k >= 1){ return wh(a/10, k); } else { return 0; } }
- */
+public class _Main2861 {
+	
+	static int count=0;
+	
+	public static void main(String[] args){
+		long start = System.currentTimeMillis();
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		sc.close();
+		
+		for(int i=a;i<=b;i++){
+			if(i%3==0){
+				count++;
+			} else {
+				pandoc(i);
+			}
+		}
+		
+		System.out.println(count % 20150523);
+		long end = System.currentTimeMillis();
+
+		System.out.println( "실행 시간 : " + ( end -	 start )/1000.0 ); 
+
+	}
+
+	private static void pandoc(int i) { // 12
+		int j = i % 10;
+		if(j == 3 || j == 6 || j == 9){
+			count++;
+		} else if(i>=10) {
+			pandoc(i/10);
+		}
+	}
+} // 수행시간 O(n)*/
